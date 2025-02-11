f4addf5d9627a8b1954a6ed291c6b54163fdc7f9
Jack Turcotte <jack.turcotte@stu.ocsb.ca>
2025-02-11 01:12:50 +0000

    Created

---
+++ hello_world_updated.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    // Name
+    std::string prefix = "Programming is ";
+    std::string opinion = "fun";
+
+    std::cout << "Hello Ms.Raffin!" << std::endl;
+    std::cout << prefix + opinion << std::endl;
+}
